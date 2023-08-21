c87d11bf7ed8b87376c6a63a8630acefe8cd7e99
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-08-20 21:29:52 -0300

    studying Java for College

---
+++ Java/HelloWorldJava/src/Pessoa.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ricardo
+ */
+public class Pessoa {
+    String sexo;
+    String corPele;
+    int peso;
+    
+    public void comer()
+    {
+        System.out.println("Estou com fome!");
+    }
+    
+    public void sono()
+    {
+        System.out.println("Estou com sono");
+    }
+    
+    public void estudar ()
+    {
+        System.out.println("0/a estudante vai estudar");
+    }
+    
+    private void correr()
+    {
+        System.out.println("Necessário correr para exercitar");
+    }
+    
+}
